98cdfd10e70e4e08053a96c834533fd609cc91db
Hubina Balázs <hubinab@gmail.com>
2025-12-18 18:49:31 +0100

    api kesz, tabla elokeszitese kesz

---
+++ backend/app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRegistrationRequest;
+use App\Http\Requests\UpdateRegistrationRequest;
+use App\Models\Registration;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRegistrationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Registration $registration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRegistrationRequest $request, Registration $registration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Registration $registration)
+    {
+        //
+    }
+}
